253967f5d1d3dcb0f6166bc3defda57cbdeb70f1
Alexander Obi <alexebube@gmail.com>
2015-12-08 11:28:44 -0500

    add sql schema to the maria db add sample data to the table

---
+++ sql/schema.sql
@@ -0,0 +1,19 @@
+
+DROP TABLE IF EXISTS `Users`;
+
+CREATE TABLE `Users` (
+	`userID` INT NOT NULL AUTO_INCREMENT,
+	`firstName` VARCHAR(255) NOT NULL,
+	`middleName` VARCHAR(255) ,
+	`lastName` VARCHAR(255) NOT NULL,
+	`email` VARCHAR(255) NOT NULL,
+	`phoneNumber` VARCHAR(255) NOT NULL,
+	`streetAddress` VARCHAR(512),
+	`city` VARCHAR(255) NOT NULL,
+	`state` VARCHAR(255) NOT NULL,
+	`zipCode` VARCHAR(255) NOT NULL,
+	`username` VARCHAR(255) NOT NULL,
+	`password` VARCHAR(255) NOT NULL,
+	`createdOn` datetime NOT NULL,
+	PRIMARY KEY(`userID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
