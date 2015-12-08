7f3a4fda08ed9204a574659c9300fa1909b7cab2
Alexander Obi <alexebube@gmail.com>
2015-12-08 11:30:53 -0500

    add grunt js file for maging database setup

---
+++ Gruntfile.js
@@ -0,0 +1,33 @@
+module.exports = function(grunt){
+
+	grunt.loadNpmTasks('grunt-mysql-runfile');
+
+	grunt.initConfig({
+
+		db: grunt.file.readJSON('config/config.json'),
+
+		mysqlrunfile: {
+			options: {
+				connection: {
+					host: '<%= db.database.host %>',
+					port: '<%= db.database.port %>',
+					user: '<%= db.database.username %>',
+					password: '<%= db.database.password %>',
+					database: '<%= db.database.database %>',
+					multipleStatements: true
+				}
+			},
+			tables: {
+                src: ['sql/schema.sql']
+            },
+            sample: {
+            	src: ['sql/sampleDB.sql']
+            }
+		}
+	});
+
+	grunt.registerTask('db:setup','Setting up SQL Schema and adding sample data',[
+		'mysqlrunfile:tables',
+		'mysqlrunfile:sample'
+	]);
+};
