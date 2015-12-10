31327728ba9f3c97c83d9b4022a962f640c5d33d
Alexander Obi <alexebube@gmail.com>
2015-12-10 11:51:48 -0500

    add addtional configuration to requirejs-config file

--- app/requirejs-config.js
+++ app/requirejs-config.js
@@ -7,7 +7,14 @@ function RequireJSConfig(){
 		paths: {
 			'main':'server',
 			'module':'server/modules',
-			'controller':'server/modules/controllers'
+			'controller':'server/modules/controllers',
+			'models':'server/modules/models',
+			'config': '../config/config.json',
+			'helper': 'server/modules/helpers',
+			'data': 'server/modules/services/data',
+			'service': 'server/modules/services/service',
+			'text': '../node_modules/requirejs-plugins/lib/text',
+			'json': '../node_modules/requirejs-plugins/src/json',
 		},
 		nodeRequire: require
 	}
