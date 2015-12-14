f86b036f59ca96e3bdb819311e37c9a5659e8520
Alexander Obi <alexebube@gmail.com>
2015-12-14 16:21:42 -0500

    add controller fn for a new api end point

--- app/server/modules/controllers/api/controller.js
+++ app/server/modules/controllers/api/controller.js
@@ -2,6 +2,17 @@ define(['service/user_service'], function(userService){
 	
 	function findAll(req, res, next) {
 		userService.getUsers().then(function(results){
+			res.send(results);
+			next();
+		}).catch(function(error){
+			next(error);
+		});
+	}
+
+	function findOne(req, res, next){
+		var id = req.params.id;
+
+		userService.getUser(id).then(function(results){
 			console.log(results);
 			res.send(results);
 			next();
@@ -10,6 +21,9 @@ define(['service/user_service'], function(userService){
 		});
 	}
 
-	return findAll;
+	return {
+		"findAll": findAll,
+		"findOne": findOne,
+	} 
 
 });
