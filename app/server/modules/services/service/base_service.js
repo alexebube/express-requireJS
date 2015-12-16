10d7e7e5c1a67eebc156c06772efa1e63671732a
Alexander Obi <alexebube@gmail.com>
2015-12-15 23:44:55 -0500

    add jsdoc comments to base_service.js

--- app/server/modules/services/service/base_service.js
+++ app/server/modules/services/service/base_service.js
@@ -1,5 +1,17 @@
+/**
+ * Base Service module that defines basic service
+ * 
+ * @module service/base_service
+ */
 define([], function(){
 
+	/**
+	 * [BaseService description]
+	 *
+	 * @constructor
+	 * @param {object} dataService - sequelize data model
+	 * @param {object} options     - options object
+	 */
 	var BaseService = function(dataService, options){
 		this.getDataService = function(){
 			return dataService;
@@ -9,6 +21,13 @@ define([], function(){
 	BaseService.prototype.get = function(){
 		var dataService = this.getDataService();
 		return dataService.find().then(function(result){
+			return result;
+		});
+	};
+
+	BaseService.prototype.getById = function(id){
+		var dataService = this.getDataService();
+		return dataService.findById(id).then(function(result){
 			return result;
 		});
 	};
