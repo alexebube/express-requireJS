270e709a01ab997f5b0e76ed566f0fa45a9e56a3
Alexander Obi <alexebube@gmail.com>
2015-12-15 23:46:01 -0500

    add getUser(id) method to retrieve a user add jsdoc comments to user_service.js

--- app/server/modules/services/service/user_service.js
+++ app/server/modules/services/service/user_service.js
@@ -32,5 +32,19 @@ define([
 		return deferred.promise;
 	};
 
+	UserService.prototype.getUser = function(id){
+		var deferred = Promise.pending(), result;
+
+		this.getById(id).then(function(results){
+			result = userConverter.conversion(results);
+			deferred.resolve(result[0]);
+		})
+		.catch(function(error){
+			deferred.reject(error);
+		});
+
+		return deferred.promise;
+	};
+
 	return new UserService(dataService, {});
 });
