42b1de01e58d9d548cb81a5f17b3dee8ba17f214
Alexander Obi <alexebube@gmail.com>
2015-12-15 23:43:32 -0500

    add jsdoc comments to routes.js

--- app/server/routes.js
+++ app/server/routes.js
@@ -1,11 +1,21 @@
 /**
- * The API Routes
- * @param  {[Object]} express [description]
- * @param  {Object} _       [description]
- * @param  {[Object]} api_v1) {	var        api [description]
- * @return {[function]}         [route funtion]
+ * Main Route Module that instantiates express route
+ * 
+ * @module main/routes
+ * @param {Object} express - express module
+ * @param {Object} lodash -  lodash utility module
+ * @param {Object} api_v1 -  controller
+ * @return {function} ap1 -  route function
  */
-define(['express', 'lodash', 'controller/api/index'], function(express, _, api_v1) {
+define([
+	'express', 
+	'lodash', 
+	'controller/api/index'
+], function(
+	express,
+	 _, 
+	 api_v1
+) {
 
 	var api = express.Router();
 
