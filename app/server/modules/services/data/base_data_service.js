52ffcf6a873f91d7cd9428c15da1fd0f166c9411
Alexander Obi <alexebube@gmail.com>
2015-12-15 23:44:31 -0500

    add jsdoc comments to base_data_service.js

--- app/server/modules/services/data/base_data_service.js
+++ app/server/modules/services/data/base_data_service.js
@@ -1,8 +1,4 @@
-define([
-	
-], function(
-
-){
+define([], function(){
 
 	var BaseDataService = function(model, options) {
 		this.model = model;
@@ -15,6 +11,13 @@ define([
 	BaseDataService.prototype.find = function(options){
 		var model = this.getModel();
 		return model.findAll(options).then(function(result){
+			return result;
+		});
+	};
+
+	BaseDataService.prototype.findById = function(id){
+		var model = this.getModel();
+		return model.findById(id).then(function(result){
 			return result;
 		});
 	};
